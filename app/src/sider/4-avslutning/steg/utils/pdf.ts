55f2b3872ad5371c3b0d86204cc2ae04dc8b0857
Morten Jansrud <morten@founder.no>
2019-04-24 14:50:47 +0200

    Endre nedlasting av andre vedlegg til blob

--- app/src/sider/4-avslutning/steg/utils/pdf.ts
+++ app/src/sider/4-avslutning/steg/utils/pdf.ts
@@ -65,5 +65,7 @@ export const lastNedPDF = (pdf: string, title: string) => {
 
 export const lastNedFil = (url: string, tittel: string, filtype: string) => {
   console.log("Laster ned " + tittel);
-  FileSaver.saveAs(url, `${tittel}.${filtype}`);
+  fetch(url)
+    .then(response => response.blob())
+    .then(blob => FileSaver.saveAs(blob, `${tittel}.${filtype}`));
 };
