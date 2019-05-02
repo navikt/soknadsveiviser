e2021e65bb0c9d2fe41440cc31c4a5ac428fbf29
Morten Jansrud <morten@founder.no>
2019-05-02 10:00:04 +0200

    Disable knapp dersom brukeren ikke har tatt stilling til alle spørsmålene

--- app/src/sider/3-soknadstilpasser/felles/personalia/knapper/Neste.tsx
+++ app/src/sider/3-soknadstilpasser/felles/personalia/knapper/Neste.tsx
@@ -1,13 +1,48 @@
 import * as React from "react";
 import { Knapp } from "nav-frontend-knapper";
 import { FormattedMessage } from "react-intl";
+import { Store } from "../../../../../typer/store";
+import { Vedleggsobjekt } from "../../../../../typer/vedlegg";
+import { Soknadsobjekt } from "../../../../../typer/soknad";
+import { medValgtSoknadsobjekt } from "../../../../../states/providers/ValgtSoknadsobjekt";
+import { connect } from "react-redux";
 
-const NesteKnapp = () => (
-  <div className="knapp__neste">
-    <Knapp className="knapp knapp--hoved sentrert__knapp row" htmlType="submit">
-      <FormattedMessage id="personalia.knapp" />
-    </Knapp>
-  </div>
+interface ReduxProps {
+  valgteVedlegg: Vedleggsobjekt[];
+}
+
+interface ValgtSoknad {
+  valgtSoknadsobjekt: Soknadsobjekt;
+  klageSoknadsobjekt?: Soknadsobjekt;
+}
+
+type MergedProps = ReduxProps & ValgtSoknad;
+const NesteKnapp = (props: MergedProps) => {
+  const relevanteVedlegg = props.valgteVedlegg
+    .filter(vedlegg => vedlegg.soknadsobjektId == props.valgtSoknadsobjekt._id)
+    .filter(vedlegg => !vedlegg.pakrevd);
+
+  const vedleggSvart = relevanteVedlegg.filter(
+    vedlegg => vedlegg.skalSendes !== undefined
+  );
+
+  return (
+    <div className="knapp__neste">
+      <Knapp
+        className="knapp knapp--hoved sentrert__knapp row"
+        htmlType="submit"
+        disabled={relevanteVedlegg.length !== vedleggSvart.length}
+      >
+        <FormattedMessage id="personalia.knapp" />
+      </Knapp>
+    </div>
+  );
+};
+
+const mapStateToProps = (store: Store) => ({
+  valgteVedlegg: store.vedlegg.valgteVedlegg
+});
+
+export default medValgtSoknadsobjekt<ValgtSoknad>(
+  connect(mapStateToProps)(NesteKnapp)
 );
-
-export default NesteKnapp;
