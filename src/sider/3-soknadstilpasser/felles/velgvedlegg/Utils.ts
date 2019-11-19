4912196203a18c554f2ee84a80824473f6bf9e03
Morten Jansrud <morten.jansrud@nav.no>
2019-11-19 15:16:54 +0100

    Ta med siste element

--- src/sider/3-soknadstilpasser/felles/velgvedlegg/Utils.ts
+++ src/sider/3-soknadstilpasser/felles/velgvedlegg/Utils.ts
@@ -4,7 +4,7 @@ export const scrollTilNesteSpm = (hash: string, vedlegg: Vedleggsobjekt[]) => {
   // Kun scroll til neste spørsmål dersom det ikke er besvart
   const nesteSpmElement = document.getElementById(hash);
   const nesteSpmNummer = parseInt(hash.split("#")[1], 10);
-  const nesteEksisterer = nesteSpmNummer < vedlegg.length;
+  const nesteEksisterer = nesteSpmNummer <= vedlegg.length;
 
   if (nesteSpmElement && nesteEksisterer) {
     // Gå til liste
