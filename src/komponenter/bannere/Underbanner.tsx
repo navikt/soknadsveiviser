e6a0a29d7692186b114abd326ec5c984f7a89958
Morten Jansrud <morten.jansrud@nav.no>
2019-11-29 12:40:51 +0100

    Endre til normaltekst

--- src/komponenter/bannere/Underbanner.tsx
+++ src/komponenter/bannere/Underbanner.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 import Normaltekst from "nav-frontend-typografi/lib/normaltekst";
-import Element from "nav-frontend-typografi/lib/element";
 import Undertittel from "nav-frontend-typografi/lib/undertittel";
 
 interface Props {
   tittel: string;
   undertittel?: string;
-  skjemanummer: string;
+  skjemanummer?: string;
 }
 
 const Underbanner = (props: Props) => {
@@ -14,7 +13,7 @@ const Underbanner = (props: Props) => {
     <div className="underbanner">
       <Undertittel>{props.tittel}</Undertittel>
       {props.undertittel && <Normaltekst>{props.undertittel}</Normaltekst>}
-      <Element>{props.skjemanummer}</Element>
+      {props.skjemanummer && <Normaltekst>{props.skjemanummer}</Normaltekst>}
     </div>
   );
 };
