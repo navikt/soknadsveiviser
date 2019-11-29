14d7048c4b04170005e826d99456876a38acd85d
Sissel Fladby <sissel.fladby@nav.no>
2019-11-29 16:37:00 +0100

    Masking av logger + update av avhengigheter

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,15 @@
 const { frontendlogger } = window as any;
 
+const mask = (error: string) => error.replace("(^|\\W)\\d{11}(?=$|\\W)", "1**********");
+
 export const loggEvent = (
   tittel: string,
   fields?: { [key: string]: any },
   tags?: { [key: string]: any }
-) => frontendlogger && frontendlogger.event(tittel, fields || {}, tags || {});
+) => frontendlogger && frontendlogger.event(mask(tittel), fields || {}, tags || {});
 
 export const loggError = (error: string) =>
-  frontendlogger && frontendlogger.error(error);
+  frontendlogger && frontendlogger.error(mask(error));
 
 export const loggApiError = (url: string, error: string, status?: number) => {
   const errorMessage = `Feil ved henting av data: ` +
@@ -17,8 +19,8 @@ export const loggApiError = (url: string, error: string, status?: number) => {
   const tags = {};
   const fields = {
     status: status || 404,
-    statusText: error,
-    url
+    statusText: mask(error),
+    url: mask(url)
   };
 
   loggError(errorMessage);
