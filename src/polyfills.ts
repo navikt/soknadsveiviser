31bebb9a4cad9f2716aeedd8a38bbb2740f55a77
Morten Jansrud <morten.jansrud@nav.no>
2020-03-09 17:06:14 +0100

    Legg til react-app-polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,4 +1,9 @@
 import "babel-polyfill";
+
+// Browsers
+require("react-app-polyfill/ie9");
+require("react-app-polyfill/ie11");
+
 import smoothscroll from "smoothscroll-polyfill";
 
 smoothscroll.polyfill();
