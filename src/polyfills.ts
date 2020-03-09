99e9e3f0c32645df5aba83879d8b708f98e4d4b7
Morten Jansrud <morten.jansrud@nav.no>
2020-03-09 17:09:31 +0100

    Reorder imports

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,9 +1,8 @@
 import "babel-polyfill";
+import smoothscroll from "smoothscroll-polyfill";
 
 // Browsers
 require("react-app-polyfill/ie9");
 require("react-app-polyfill/ie11");
-
-import smoothscroll from "smoothscroll-polyfill";
 
 smoothscroll.polyfill();
