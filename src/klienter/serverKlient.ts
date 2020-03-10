924402dc559b7b6e9c8dc7c15da7ddca49b14b0f
anders-nom <anders@nomerstad.com>
2020-03-10 09:27:55 +0100

    Legger til polyfills for IE11

--- src/klienter/serverKlient.ts
+++ src/klienter/serverKlient.ts
@@ -4,6 +4,7 @@ import { parseJson } from "./parser";
 
 export const fetchConfig = (): Promise<Config> =>
   fetch("/soknader/config")
+    // Denne sjekken er for proxy-polyfill for IE11 støtte, usikker på det er nødvendig i prod
     .then((res) => Object.isExtensible(res) ? res.json() : res.clone().json())
     .catch(console.error);
 
