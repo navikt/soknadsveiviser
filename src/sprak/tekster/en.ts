8ca6f4ca4bb5df40af50cddced18dc6f1596d752
Christian-Haslund <50137233+Christian-Haslund@users.noreply.github.com>
2020-09-04 11:11:23 +0200

    Update en.ts
    "kategori.velg" : Endret tekst fra "Menu" til "Form categories". Norsk tekst skal være "Søknadskategorier"

--- src/sprak/tekster/en.ts
+++ src/sprak/tekster/en.ts
@@ -108,7 +108,7 @@ In order for your papers to arrive quickly at the right place in NAV, enter whic
   "ettersendelser.mellomledd.anke.beskrivelse": `Choose 'Forward to an appeal' if you have appealed to the National Insurance Court and want to forward additional documentation.`,
   "sidetittel": `Applications and Forms`,
   "sideingress": `You will find options for application forms, submitting additional documentation, and complaint forms by choosing a topic below.`,
-  "kategori.velg": `Menu`,
+  "kategori.velg": `Form categories`,
   "kategori.meta_desc": `{kategoritittel}: Submit application forms, additional documentation, or complaint forms.`,
   "kategorier.knapp.bedrift": `Employers`,
   "kategorier.knapp.engelsk": `English`,
