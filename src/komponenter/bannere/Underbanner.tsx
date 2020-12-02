206e216810ee4862f94916789f4bc1e255554e6a
Wenqi Lin <wenqi.lin@nav.no>
2020-12-02 15:22:14 +0100

    reapply the old code

--- src/komponenter/bannere/Underbanner.tsx
+++ src/komponenter/bannere/Underbanner.tsx
@@ -5,12 +5,14 @@ import Undertittel from "nav-frontend-typografi/lib/undertittel";
 interface Props {
   tittel: string;
   skjemanummer?: string;
+  undertittel?: string;
 }
 
 const Underbanner = (props: Props) => {
   return (
     <div className="underbanner">
       <Undertittel>{props.tittel}</Undertittel>
+      {props.undertittel && <Normaltekst>{props.undertittel}</Normaltekst>}
       {props.skjemanummer && <Normaltekst>{props.skjemanummer}</Normaltekst>}
     </div>
   );
