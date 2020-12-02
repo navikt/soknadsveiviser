b9a1a4ed5abe9b170c03404b259172b87f3f3b3d
Wenqi Lin <wenqi.lin@nav.no>
2020-12-02 14:04:57 +0100

    remove subtitle on klageside

--- src/komponenter/bannere/Underbanner.tsx
+++ src/komponenter/bannere/Underbanner.tsx
@@ -4,7 +4,6 @@ import Undertittel from "nav-frontend-typografi/lib/undertittel";
 
 interface Props {
   tittel: string;
-  undertittel?: string;
   skjemanummer?: string;
 }
 
@@ -12,7 +11,6 @@ const Underbanner = (props: Props) => {
   return (
     <div className="underbanner">
       <Undertittel>{props.tittel}</Undertittel>
-      {props.undertittel && <Normaltekst>{props.undertittel}</Normaltekst>}
       {props.skjemanummer && <Normaltekst>{props.skjemanummer}</Normaltekst>}
     </div>
   );
