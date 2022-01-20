0198fff844be013d0a7b564e7783fa19ed830141
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2022-01-20 09:53:40 +0100

    Hente secrets fra kubernetes. En del secrets er satt til tom verdi pga. at de kommer til å være overflødig når vi har endret søknadsveiviseren til å hente enhetsinfo og generere førsteside gjennom skjemabygging-proxy.

--- server/utils/getSecrets.js
+++ server/utils/getSecrets.js
@@ -1,48 +1,15 @@
 function getSecrets() {
-  const fs = require("file-system");
-  const secretsFilePath = "/var/run/secrets/nais.io/vault";
 
-  const enheterRSURL = fs.readFileSync(
-    secretsFilePath + "/enheterRS.url",
-    "utf8"
-  );
-  const enheterRSApiKey = fs.readFileSync(
-    secretsFilePath + "/enheterRS.apiKey",
-    "utf8"
-  );
-  const sanityDataset = fs.readFileSync(
-    secretsFilePath + "/sanity.dataset",
-    "utf8"
-  );
-  const securityTokenServiceTokenUrl = fs.readFileSync(
-    secretsFilePath + "/security.token.service.token.url",
-    "utf8"
-  );
-  const securityTokenServiceTokenApiKey = fs.readFileSync(
-    secretsFilePath + "/security.token.service.token.apiKey",
-    "utf8"
-  );
-  const soknadsveiviserUser = fs.readFileSync(
-    secretsFilePath + "/soknadsveiviser.user",
-    "utf8"
-  );
-  const soknadsveiviserPass = fs.readFileSync(
-    secretsFilePath + "/soknadsveiviser.pass",
-    "utf8"
-  );
-  const foerstesidegeneratorServiceUrl = fs.readFileSync(
-    secretsFilePath + "/foerstesidegenerator.service.url",
-    "utf8"
-  );
-  const foerstesidegeneratorServiceApiKey = fs.readFileSync(
-    secretsFilePath + "/foerstesidegenerator.service.apiKey",
-    "utf8"
-  );
-
-  const soknadsveiviserproxyUrl = fs.readFileSync(
-    secretsFilePath + "/soknadsveiviserproxy.url",
-    "utf8"
-  );
+  const enheterRSURL = "";
+  const enheterRSApiKey = "";
+  const sanityDataset = process.env.SANITY_DATASET;
+  const securityTokenServiceTokenUrl = "";
+  const securityTokenServiceTokenApiKey = "";
+  const soknadsveiviserUser = "";
+  const soknadsveiviserPass = "";
+  const foerstesidegeneratorServiceUrl = "";
+  const foerstesidegeneratorServiceApiKey = "";
+  const soknadsveiviserproxyUrl = process.env.SOKNADSVEIVISERPROXY_URL
 
   return [
     enheterRSURL,
