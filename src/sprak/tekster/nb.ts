56d1c9975d16d89fb934bf2f31d7a961b91f9393
Rune Rosseland <rune.rosseland@nav.no>
2023-02-01 16:36:29 +0100

    Endret tekst på fyllut lenke

--- src/sprak/tekster/nb.ts
+++ src/sprak/tekster/nb.ts
@@ -149,7 +149,7 @@ For at papirene dine skal komme raskt fram til riktig sted i NAV, skriver du inn
   "vissoknadsobjekter.knapp.soknadsdialog": `Send digitalt`,
   "vissoknadsobjekter.knapp.dokumentinnsending": `Send digitalt`,
   "vissoknadsobjekter.knapp.fyllut": `Fyll ut`,
-  "vissoknadsobjekter.fyllUt": `Send på papir`,
+  "vissoknadsobjekter.fyllUt": `Send i posten`,
   "vissoknadsobjekter.tittel": `Digital søknad om`,
   "vissoknadsobjekter.ikkeelektroniskID": `Send på papir`,
   "feil.tekst.ingen": "Feil, fant ikke tekst.",
